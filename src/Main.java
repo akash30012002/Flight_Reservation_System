60663708c33e99447e030ee66c85322e791b41c9
Akash Sharma <akash.a.sharma04@gmail.com>
2021-04-20 23:29:27 +0530

    Checkpoint 5

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Flight indigo320 = new Flight("A11","indigo",2,0);
-		Passenger passenger = new Passenger("Akash", "26749372", "abc@gmail.com", "Sesame", "Mumbai", "Mah");
+		Flight indigo320 = new Flight("A320","indigo",5,0);
+		Passenger passenger1 = new Passenger("Akash", "9920935531", "akash@gmail.com", "Sesame street", "Mumbai", "Mah");
+		Passenger passenger2 = new Passenger("Rajat", "9892715423", "rajat@gmail.com", "Park Street", "New Delhi", "Delhi");
 		
-		Regular ticket1 = new Regular("abcd", "Delhi", "mumbai", indigo320, "1/1/2021 5:50",
-				"1/1/2021 5:50", passenger, "A1", 5400, false, "Food");
-		Regular ticket2 = new Regular("abcd", "Delhi", "mumbai", indigo320, "1/1/2021 5:50",
-				"1/1/2021 5:50", passenger, "A1", 5400, false, "Drink");
-		Regular ticket3 = new Regular("abcd", "Delhi", "mumbai", indigo320, "1/1/2021 5:50",
-				"1/1/2021 5:50", passenger, "A1", 5400, false, "Wifi");
-		
-		System.out.println(ticket1.getIsCancelled());
-		System.out.println(ticket2.getIsCancelled());
-		System.out.println(ticket3.getIsCancelled());
-		
-		System.out.println(indigo320.getSeatsBooked());
-		System.out.println(indigo320.checkAvailability());
-	}
+		Regular ticket1 = new Regular("1101", "Delhi", "mumbai", indigo320, "1/1/2021 5:50",
+				"1/1/2021 5:50", passenger1, "A1", 5400, false, "Food");
 
+		Tourist ticket4 = new Tourist("1102", "Kolkata", "mumbai", indigo320, "10/2/2021 5:50",
+				"11/2/2021 5:50", passenger2, "A1", 5400, false, "WestIn");
+		
+		
+		printTicketDetails(ticket1);
+		printTicketDetails(ticket4);
+		
+	}
+	
+	public static void printTicketDetails(Regular ticket) {	
+		System.out.println(ticket.getPnr());
+	}
+	
+	public static void printTicketDetails(Tourist ticket) {	
+		System.out.println(ticket.getPnr());
+	}
 }
--- src/Passenger.java
+++ src/Passenger.java
@@ -60,29 +60,6 @@ public class Passenger {
 			this.state = state;
 		}
 
-		public String getStreet() {
-			return this.street;
-		}
-
-		public void setStreet(String street) {
-			this.street = street;
-		}
-
-		public String getCity() {
-			return this.city;
-		}
-
-		public void setCity(String city) {
-			this.city = city;
-		}
-
-		public String getState() {
-			return this.state;
-		}
-
-		public void setState(String state) {
-			this.state = state;
-		}
 		
 	}
 	
@@ -106,30 +83,6 @@ public class Passenger {
 		public void updateContact(String name, String phoneNumber, String email) {
 			this.name = name;
 			this.phoneNumber = phoneNumber;
-			this.email = email;
-		}
-	
-		public String getName() {
-			return this.name;
-		}
-	
-		public void setName(String name) {
-			this.name = name;
-		}
-	
-		public String getPhoneNumber() {
-			return this.phoneNumber;
-		}
-	
-		public void setPhoneNumber(String phoneNumber) {
-			this.phoneNumber = phoneNumber;
-		}
-	
-		public String getEmail() {
-			return this.email;
-		}
-	
-		public void setEmail(String email) {
 			this.email = email;
 		}
 	
